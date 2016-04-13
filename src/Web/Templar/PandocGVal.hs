837c7f6fba2c97428b4c08dcee91cbe0a6cd139a
Tobias Dammers <tdammers@gmail.com>
2016-04-13 23:20:46 +0200

    Properties on inlines

--- src/Web/Templar/PandocGVal.hs
+++ src/Web/Templar/PandocGVal.hs
@@ -144,4 +144,81 @@ instance ToGVal m Alignment where
     toGVal AlignDefault = def
 
 instance ToGVal m Inline where
-    toGVal inline = toGVal (Pandoc nullMeta [Plain [inline]])
+    toGVal inline =
+        let pandoc = Pandoc nullMeta [Plain [inline]]
+            listItems =
+                (map toGVal $ (query (:[]) inline :: [Inline])) ++
+                (map toGVal $ (query (:[]) inline :: [Block]))
+        in def { asList = Just listItems
+               , asDictItems = Just $ mapToList (inlineProperties inline)
+               , asLookup = Just $ \key -> lookup key (inlineProperties inline)
+               , asHtml = unsafeRawHtml . pack . writeHtmlString writerOptions $ pandoc
+               , asText = pack . writePlain writerOptions $ pandoc
+               , asBoolean = True
+               , asNumber = Nothing
+               , asFunction = Nothing
+               , Ginger.length = Just (ClassyPrelude.length listItems)
+               , isNull = False
+               }
+
+inlineProperties :: forall m. Inline -> HashMap Text (GVal m)
+inlineProperties (Str _) = mapFromList ["type" ~> ("str" :: Text)]
+inlineProperties (Emph _) = mapFromList ["type" ~> ("em" :: Text)]
+inlineProperties (Strong _) = mapFromList ["type" ~> ("strong" :: Text)]
+inlineProperties (Strikeout _) = mapFromList ["type" ~> ("strikeout" :: Text)]
+inlineProperties (Superscript _) = mapFromList ["type" ~> ("superscript" :: Text)]
+inlineProperties (Subscript _) = mapFromList ["type" ~> ("subscript" :: Text)]
+inlineProperties (SmallCaps _) = mapFromList ["type" ~> ("smallCaps" :: Text)]
+inlineProperties (Quoted quoteType _) = mapFromList ["type" ~> ("quoted" :: Text)]
+inlineProperties (Cite citations _) =
+    mapFromList
+        [ "type" ~> ("cite" :: Text)
+        , "citations" ~> citations
+        ]
+inlineProperties (Code (id, classes, attrs) _) =
+    mapFromList
+        [ "type" ~> ("code" :: Text)
+        , "id" ~> id
+        , "classes" ~> classes
+        , "attrs" ~> attrs
+        , ("attrs", dict [ pack t ~> v | (t, v) <- attrs ])
+        ]
+inlineProperties Space = mapFromList ["type" ~> ("space" :: Text)]
+inlineProperties LineBreak = mapFromList ["type" ~> ("br" :: Text)]
+inlineProperties (Math mathType _) = mapFromList ["type" ~> ("math" :: Text)]
+inlineProperties (RawInline fmt _) = mapFromList ["type" ~> ("rawInline" :: Text)]
+inlineProperties (Link (id, classes, attrs) _ target) =
+    mapFromList
+        [ "type" ~> ("link" :: Text)
+        , "id" ~> id
+        , "classes" ~> classes
+        , ("attrs", dict [ pack t ~> v | (t, v) <- attrs ])
+        ]
+inlineProperties (Image (id, classes, attrs) _ target) =
+    mapFromList
+        [ "type" ~> ("image" :: Text)
+        , "id" ~> id
+        , "classes" ~> classes
+        , ("attrs", dict [ pack t ~> v | (t, v) <- attrs ])
+        ]
+inlineProperties (Note _) = mapFromList ["type" ~> ("note" :: Text)]
+inlineProperties (Span (id, classes, attrs) _) =
+    mapFromList
+        [ "type" ~> ("span" :: Text)
+        , "id" ~> id
+        , "classes" ~> classes
+        , ("attrs", dict [ pack t ~> v | (t, v) <- attrs ])
+        ]
+
+instance ToGVal m Citation where
+    toGVal c =
+        dict [ "id" ~> citationId c
+             , "prefix" ~> citationPrefix c
+             , "suffix" ~> citationSuffix c
+             , "mode" ~> citationMode c
+             , "noteNum" ~> citationNoteNum c
+             , "hash" ~> citationHash c
+             ]
+
+instance ToGVal m CitationMode where
+    toGVal = toGVal . show
