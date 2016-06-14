44984cb87410b4659d0fbc3ddc364f6d58374ff7
Tobias Dammers <tdammers@gmail.com>
2016-06-14 18:30:05 +0200

    CLI arg parser, properly

--- app/Main.hs
+++ app/Main.hs
@@ -8,38 +8,81 @@ import ClassyPrelude
 import Web.Templar
 import Text.Read (read, readMaybe)
 import Data.Default (def)
+import Text.Parsec
 
 parseArgs :: [Text] -> IO ServerConfig
-parseArgs = parseArgs' . reverse
+parseArgs argv = do
+    let result = runParser argsP () "command line arguments" argv
+    fns <- either
+        (fail . show)
+        return
+        result
+    return $ foldr ($) def fns
 
-parseArgs' :: [Text] -> IO ServerConfig
-parseArgs' [] = return def
-parseArgs' ("-warp":xs) = do
-    let portMay = readMaybe . unpack =<< headMay xs
-    case portMay of
-        Nothing -> do
-            rhs <- parseArgs xs
-            return $ rhs { scDriver = WarpDriver 5000 }
-        Just port -> do
-            rhs <- parseArgs (drop 1 xs)
-            return $ rhs { scDriver = WarpDriver port }
-parseArgs' ("-cgi":xs) = do
-    rhs <- parseArgs xs
-    return $ rhs { scDriver = CGIDriver }
-parseArgs' ("-scgi":xs) = do
-    rhs <- parseArgs xs
-    return $ rhs { scDriver = SCGIDriver }
-parseArgs' ("-fcgi":xs) = do
-    rhs <- parseArgs xs
-    return $ rhs { scDriver = FastCGIDriver }
-parseArgs' (x:xs) = do
-    let portMay = readMaybe . unpack $ x
-    case portMay of
-        Nothing ->
-            fail $ "Unknown argument: " ++ unpack x
-        Just port -> do
-            rhs <- parseArgs (drop 1 xs)
-            return $ rhs { scDriver = WarpDriver port }
+type ArgsP = Parsec [Text] ()
+
+data ArgSpec a = Flag Text a
+               | Optional Text (Maybe Text -> Maybe a)
+               | Required Text (Text -> Maybe a)
+               | Bare (Text -> Maybe a)
+
+argsP :: ArgsP [ServerConfig -> ServerConfig]
+argsP = Text.Parsec.many $ choice (map (Text.Parsec.try . argP) argSpecs)
+
+tSatisfy :: (Show t, Stream s m t) => (t -> Bool) -> ParsecT s u m t
+tSatisfy cond = do
+    actual <- anyToken
+    if cond actual
+        then return actual
+        else fail $ "unexpected " ++ show actual
+
+tExactly :: (Show t, Stream s m t, Eq t) => t -> ParsecT s u m t
+tExactly expected = tSatisfy (== expected)
+
+isFlag :: Text -> Bool
+isFlag = ("-" `isPrefixOf`)
+
+isNotFlag :: Text -> Bool
+isNotFlag = not . isFlag
+
+argP :: ArgSpec a -> ArgsP a
+argP (Flag str x) = do
+    tExactly ("-" <> str)
+    return x
+argP (Optional str f) = do
+    tExactly ("-" <> str)
+    paramMay <- optionMaybe (tSatisfy isNotFlag)
+    maybe
+        (fail "invalid parameter")
+        return
+        (f paramMay)
+argP (Required str f) = do
+    tExactly ("-" <> str)
+    param <- tSatisfy isNotFlag
+    maybe
+        (fail "invalid parameter")
+        return
+        (f param)
+argP (Bare f) =
+    (f <$> tSatisfy isNotFlag) >>= maybe (fail "invalid bare argument") return
+
+argSpecs :: [ArgSpec (ServerConfig -> ServerConfig)]
+argSpecs =
+    [ Optional
+        "warp"
+        (maybe
+            (Just $ \config -> config { scDriver = WarpDriver 5000 })
+            (\str -> do
+                port <- readMaybe . unpack $ str
+                return $ \config -> config { scDriver = WarpDriver port })
+        )
+    , Bare (\str -> do
+                port <- readMaybe . unpack $ str
+                return $ \config -> config { scDriver = WarpDriver port })
+    , Flag "cgi" (\config -> config { scDriver = CGIDriver })
+    , Flag "scgi" (\config -> config { scDriver = SCGIDriver })
+    , Flag "fcgi" (\config -> config { scDriver = FastCGIDriver })
+    ]
 
 main :: IO ()
 main = do
@@ -47,5 +90,5 @@ main = do
     sconfig' <- loadServerConfig "."
     sconfig <- parseArgs args
 
-    project <- loadProject sconfig "."
+    project <- loadProject (sconfig' `mappend` sconfig) "."
     serveProject sconfig project
